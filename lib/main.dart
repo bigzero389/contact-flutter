601e6338808801c918554750d0181c4abdc5f5c8
bigzero <garack@gmail.com>
2022-01-17 22:51:04 +0900

    chapter 2 finished

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Container(width: 50, height: 50, color: Colors.blue),
+      // home: Center(
+      //   child: Container(width: 50, height: 50, color: Colors.blue),
+      // )
+      // home: Scaffold(
+      //   body: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       Icon(Icons.star),
+      //       Icon(Icons.star),
+      //       Icon(Icons.star),
+      //     ]
+      //   ),
+      // ),
+      // homework chapter 2
+      home: Scaffold(
+        // appBar: AppBar( title: Row( children: [ Text('앱임') ] ) ),
+        appBar: AppBar( title: Text('앱임') ),
+        body: Text('안녕'),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page)
+            ],
+          )
+        ),
       )
     );
   }
