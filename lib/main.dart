b52831bfdd6eb0ccb558be7cb6e79abf04b71961
bigzero <garack@gmail.com>
2022-01-22 13:33:48 +0900

    finish chapter 03

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // homework chapter 2
       home: Scaffold(
-        // appBar: AppBar( title: Row( children: [ Text('앱임') ] ) ),
         appBar: AppBar( title: Text('앱임') ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text('안녕'),
-            Icon(Icons.star),
-          ]
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            child: Text('여백들'),
+            width: double.infinity/* 바깥쪽 Container 내에서 꽉차게 넣음 */, height: 100, /* color: Colors.black,*/
+            // margin: EdgeInsets.all(20),  // 바깥쪽 여백
+            margin: EdgeInsets.fromLTRB(10, 20, 10, 30),
+            padding: EdgeInsets.all(20),  // 안쪽 여백
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black), // 동일한 Container 안에 Color 를 두개 넣으면 에러난다.
+            ),
+          ),
         ),
         bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page)
-            ],
+          child: SizedBox ( //Container( // Container 보다 SizedBox 가 더 가볍다.
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)
+              ],
+            ),
           )
         ),
       )
