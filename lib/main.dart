0decb8f5db7db664f7904277132ee1ddae0a47dc
bigzero <garack@gmail.com>
2022-01-22 14:13:19 +0900

    finish chapter 04

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar( title: Text('앱임') ),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            child: Text('여백들'),
-            width: double.infinity/* 바깥쪽 Container 내에서 꽉차게 넣음 */, height: 100, /* color: Colors.black,*/
-            // margin: EdgeInsets.all(20),  // 바깥쪽 여백
-            margin: EdgeInsets.fromLTRB(10, 20, 10, 30),
-            padding: EdgeInsets.all(20),  // 안쪽 여백
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black), // 동일한 Container 안에 Color 를 두개 넣으면 에러난다.
-            ),
+        // appBar: AppBar(leading: Icon(Icons.star), title: Text('타이틀'),),
+        appBar: AppBar( actions: [Icon(Icons.star), Icon(Icons.star)], title: Text('타이틀') ),
+        // 레이아웃 잘짜는 법
+        // 1. 원화가 필요함(원본디자인), 없으면 베낀다
+        // 2. 네모부터 그린다. 빈공간이 없도록
+        // 3. 겉에 박스부터 안에까지 차례로 container 나 row 들을 배치한다.
+        // 4. 마지막으로 디자인(패딩, 마진등)을 한다.
+        body: SizedBox(
+          child: Row(
+            children: [
+              Text('안녕하세요', style: TextStyle(color: Colors.red),),
+              Icon(Icons.star, color: Colors.amber,),
+              ElevatedButton(
+                child: Text('글자'),
+                onPressed: (){},)
+            ],
           ),
         ),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox ( //Container( // Container 보다 SizedBox 가 더 가볍다.
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page)
-              ],
-            ),
-          )
-        ),
-      )
+      ),
     );
   }
 }
