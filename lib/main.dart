3293cea9be5078301f1d652701fc685b216d3f10
bigzero <garack@gmail.com>
2022-01-22 20:23:52 +0900

    custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // appBar: AppBar(leading: Icon(Icons.star), title: Text('타이틀'),),
-        appBar: AppBar( actions: [Icon(Icons.star), Icon(Icons.star)], title: Text('타이틀') ),
-        // 레이아웃 잘짜는 법
-        // 1. 원화가 필요함(원본디자인), 없으면 베낀다
-        // 2. 네모부터 그린다. 빈공간이 없도록
-        // 3. 겉에 박스부터 안에까지 차례로 container 나 row 들을 배치한다.
-        // 4. 마지막으로 디자인(패딩, 마진등)을 한다.
-        body: SizedBox(
-          width: double.infinity,
-          height: 150,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Flexible(child: Image.asset('assets/dog.png', width: 200,), flex: 3,),
-              Flexible(child: SizedBox(
-                height: 150,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('멍멍이', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
-                    Text('용마산로 174'),
-                    Text('70,000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ],
-                    )
-                  ],
-                ),
-                ), flex: 7,
-              )
-            ]
-          )
-        )
-      ),
+        appBar: AppBar(),
+        // 아무거나 다 커스텀 위젯으로 만들면 안됨. 재사용 많거나 큰 페이들을 커스텀 위젯으로 만드는 것이 좋음.
+        body: ShopItem(),
+      )
     );
   }
 }
+
+// stless
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('Hello Custom Widget!!!'),
+    );
+  }
+}
+
